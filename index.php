2c61055a1bbe8d9488cdd68800300fefad0c3121
Léo <bacquartl@icloud.com>
2023-05-29 18:09:55 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<form action="handle_login.php" method="post">
+    <label for="username">Username : </label>
+    <input type="text" name="username" id="username">
+    <br><br>
+    <label for="password">Password : </label>
+    <input type="password" name="password" id="password">
+    <br><br>
+    <input type="submit" content="Submit">
+</form>
+</body>
+</html>
